b0870a84433da7631afe17ffb03b1a8960756c6e
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-10-05 07:33:53 -0500

    Added Shoe class

---
+++ Shoe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeStore
+{
+    internal class Shoe
+    {
+        public Style Style { get; private set; }
+        public string Color { get; private set; }
+        public string Description { get { return $"A {Color} {Style}"; } }
+        public Shoe(Style style, string color)
+        {
+            this.Style = style;
+            this.Color = color;
+        }
+
+    }
+
+}
