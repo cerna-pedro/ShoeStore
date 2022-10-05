7dfb884d01baf8bc8651c600b0c4cf5cd05b4a02
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-10-05 12:21:04 -0500

    Added Program.Main method

--- Program.cs
+++ Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+        static ShoeCloset shoeCloset = new ShoeCloset();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                shoeCloset.PrintShoes();
+                Console.Write($"\nPress 'a' to add or 'r' to remove a shoe: ");
+                char key = Console.ReadKey().KeyChar;
+                switch (key)
+                {
+                    case 'a':
+                    case 'A':
+                        shoeCloset.AddShoe();
+                        break;
+                    case 'r':
+                    case 'R':
+                        shoeCloset.RemoveShoe();
+                        break;
+                    default:
+                        return;
+                }
+            }
         }
     }
 }
